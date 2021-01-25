902b1651abe812b7e0c39777592a9220810ee8d0
kimjunseok <jnunseok@gmail.com>
2021-01-25 17:18:22 +0900

    feat: Post domain 추가

---
+++ spring-swagger/src/main/java/spring/swagger/domain/Post.java
@@ -0,0 +1,36 @@
+package spring.swagger.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 25)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    protected Post() {}
+
+    public Post(final String title, final String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
