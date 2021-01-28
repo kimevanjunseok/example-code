c192844ad589056408cca6fb5b9be426c52e84e6
kimjunseok <jnunseok@gmail.com>
2021-01-28 15:05:19 +0900

    feat: PostRepository 생성

--- spring-rest-docs/src/main/java/spring/restdocs/controller/PostController.java
+++ spring-rest-docs/src/main/java/spring/restdocs/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import spring.restdocs.dto.PostRequest;
+import spring.restdocs.dto.PostResponse;
 import spring.restdocs.service.PostService;
 
 @RequestMapping("/posts")
--- spring-rest-docs/src/main/java/spring/restdocs/service/PostService.java
+++ spring-rest-docs/src/main/java/spring/restdocs/service/PostService.java
@@ -2,6 +2,18 @@ package spring.restdocs.service;
 
 import org.springframework.stereotype.Service;
 
+import spring.restdocs.dto.PostRequest;
+import spring.restdocs.dto.PostResponse;
+import spring.restdocs.repository.PostRepository;
+
 @Service
 public class PostService {
+
+    private PostRepository postRepository;
+
+
+
+    public PostResponse create(PostRequest postRequest) {
+        return null;
+    }
 }
