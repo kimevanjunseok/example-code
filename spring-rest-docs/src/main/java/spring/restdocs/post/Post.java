d99ac36aaada41026df3cd3eec98bece6f323944
kimjunseok <jnunseok@gmail.com>
2021-01-28 14:54:45 +0900

    docs: Post domain 생성

---
+++ spring-rest-docs/src/main/java/spring/restdocs/post/Post.java
@@ -0,0 +1,35 @@
+package spring.restdocs.post;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 25)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    protected Post() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
