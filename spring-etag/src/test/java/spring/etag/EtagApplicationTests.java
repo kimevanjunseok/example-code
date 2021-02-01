d6e503a451db095e1c19153287afeb45b2113536
kimjunseok <jnunseok@gmail.com>
2021-02-01 19:25:15 +0900

    feat: spring etag init

---
+++ spring-etag/src/test/java/spring/etag/EtagApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.etag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
