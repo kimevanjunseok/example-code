d5a7787bed82b609b3bb4054065c7fc1363fe71d
kimjunseok <jnunseok@gmail.com>
2021-02-08 21:18:01 +0900

    feat: post create 기능 구현

--- spring-argumentresolver/src/main/java/spring/argumentresolver/controller/PostController.java
+++ spring-argumentresolver/src/main/java/spring/argumentresolver/controller/PostController.java
@@ -1,7 +1,13 @@
 package spring.argumentresolver.controller;
 
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import spring.argumentresolver.dto.PostRequest;
+import spring.argumentresolver.dto.PostResponse;
 import spring.argumentresolver.service.PostService;
 
 @RestController
@@ -11,5 +17,10 @@ public class PostController {
 
     public PostController(final PostService postService) {
         this.postService = postService;
+    }
+
+    public ResponseEntity<PostResponse> create(@RequestBody final PostRequest postRequest) {
+        final PostResponse postResponse = postService.create(postRequest);
+        return ResponseEntity.created(URI.create("/posts/" + postResponse.getId())).body(postResponse);
     }
 }
--- spring-argumentresolver/src/main/java/spring/argumentresolver/service/PostService.java
+++ spring-argumentresolver/src/main/java/spring/argumentresolver/service/PostService.java
@@ -2,6 +2,9 @@ package spring.argumentresolver.service;
 
 import org.springframework.stereotype.Service;
 
+import spring.argumentresolver.domain.Post;
+import spring.argumentresolver.dto.PostRequest;
+import spring.argumentresolver.dto.PostResponse;
 import spring.argumentresolver.repository.PostRepository;
 
 @Service
@@ -11,5 +14,10 @@ public class PostService {
 
     public PostService(final PostRepository postRepository) {
         this.postRepository = postRepository;
+    }
+
+    public PostResponse create(final PostRequest postRequest) {
+        final Post post = postRepository.save(postRequest.toEntity());
+        return PostResponse.of(post);
     }
 }
