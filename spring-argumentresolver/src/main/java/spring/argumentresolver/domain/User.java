9ca4991a03facdb446d8405c7f9de9e6dd4d1483
kimjunseok <jnunseok@gmail.com>
2021-02-08 21:20:03 +0900

    feat: User 생성

---
+++ spring-argumentresolver/src/main/java/spring/argumentresolver/domain/User.java
@@ -0,0 +1,35 @@
+package spring.argumentresolver.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String identification;
+
+    @Column(nullable = false)
+    private String password;
+
+    protected User() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getIdentification() {
+        return identification;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
