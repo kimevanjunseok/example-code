84905cbe0c7763cfa04d2a924b69278094226544
kimjunseok <jnunseok@gmail.com>
2021-02-17 15:09:16 +0900

    feat: Post 도메인 생성

---
+++ spring-querydsl/src/main/java/spring/querydsl/domain/Post.java
@@ -0,0 +1,35 @@
+package spring.querydsl.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    protected Post() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
