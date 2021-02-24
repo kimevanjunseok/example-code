09bd66d0db278a12b28e644ccdc2027483042731
kimjunseok <jnunseok@gmail.com>
2021-02-24 23:12:02 +0900

    feat: spring dynamic test init

---
+++ spring-dynamic-test/src/main/java/spring/dynamictest/DynamictestApplication.java
@@ -0,0 +1,13 @@
+package spring.dynamictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamictestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamictestApplication.class, args);
+    }
+
+}
