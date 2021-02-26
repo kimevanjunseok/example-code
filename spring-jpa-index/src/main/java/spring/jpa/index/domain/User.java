6640583e071b7c119a4494c4e71c211e1c094457
kimjunseok <jnunseok@gmail.com>
2021-02-26 14:15:28 +0900

    feat: User domain 추가

---
+++ spring-jpa-index/src/main/java/spring/jpa/index/domain/User.java
@@ -0,0 +1,48 @@
+package spring.jpa.index.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String name;
+
+    protected User() {}
+
+    public User(final String email, final String password, final String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
