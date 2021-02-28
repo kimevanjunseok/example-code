5a9cd5bd6f0b057012d9dc4d413ab7a698277953
kimjunseok <jnunseok@gmail.com>
2021-02-28 17:00:57 +0900

    fix: 안 쓰는 import 제거

--- spring-jpa-index/src/test/java/spring/jpa/index/service/UserServiceTest.java
+++ spring-jpa-index/src/test/java/spring/jpa/index/service/UserServiceTest.java
@@ -2,26 +2,19 @@ package spring.jpa.index.service;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.StopWatch;
 
-import spring.jpa.index.domain.User;
 import spring.jpa.index.dto.UserResponse;
-import spring.jpa.index.repository.UserRepository;
 
 @SpringBootTest
 class UserServiceTest {
 
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private UserRepository userRepository;
 
     private StopWatch stopWatch;
 
