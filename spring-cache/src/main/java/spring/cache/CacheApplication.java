32f18b0e08a4b7cfd23660b9c51994ac11c223e9
kimjunseok <jnunseok@gmail.com>
2021-04-03 21:53:56 +0900

    init: spring cache

---
+++ spring-cache/src/main/java/spring/cache/CacheApplication.java
@@ -0,0 +1,13 @@
+package spring.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheApplication.class, args);
+    }
+
+}
