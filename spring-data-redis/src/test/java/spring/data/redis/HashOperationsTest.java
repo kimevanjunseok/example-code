d04f5fbb89eb9646e890ae3f7ba522218c6bf856
kimjunseok <jnunseok@gmail.com>
2021-04-09 20:33:04 +0900

    fix: HashOperationsTest assertAll

--- spring-data-redis/src/test/java/spring/data/redis/HashOperationsTest.java
+++ spring-data-redis/src/test/java/spring/data/redis/HashOperationsTest.java
@@ -1,6 +1,7 @@
 package spring.data.redis;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import javax.annotation.Resource;
 
@@ -22,26 +23,34 @@ class HashOperationsTest extends RedisTest {
         hashOperations.increment(HASH_OPERATIONS_KEY, "count1", 1);
         hashOperations.increment(HASH_OPERATIONS_KEY, "count2", 2);
 
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "count1")).isEqualTo("2");
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "count2")).isEqualTo("3");
+        assertAll(
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "count1")).isEqualTo("2"),
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "count2")).isEqualTo("3")
+        );
+
 
         // put
         hashOperations.put(HASH_OPERATIONS_KEY, "tigger1", "개발자");
         hashOperations.put(HASH_OPERATIONS_KEY, "tigger2", "취준생");
         hashOperations.put(HASH_OPERATIONS_KEY, "tigger3", "백수");
 
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger1")).isEqualTo("개발자");
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger2")).isEqualTo("취준생");
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger3")).isEqualTo("백수");
+        assertAll(
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger1")).isEqualTo("개발자"),
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger2")).isEqualTo("취준생"),
+                () ->assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger3")).isEqualTo("백수")
+        );
 
         // putIfAbsent
         Boolean tigger1 = hashOperations.putIfAbsent(HASH_OPERATIONS_KEY, "tigger1", "요리사");
         Boolean notExist = hashOperations.putIfAbsent(HASH_OPERATIONS_KEY, "notExist", "개발자");
 
-        assertThat(tigger1).isFalse();
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger1")).isEqualTo("개발자");
-        assertThat(notExist).isTrue();
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "notExist")).isEqualTo("개발자");
+        assertAll(
+                () -> assertThat(tigger1).isFalse(),
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger1")).isEqualTo("개발자"),
+                () -> assertThat(notExist).isTrue(),
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "notExist")).isEqualTo("개발자")
+        );
+
 
         // size
         Long size = hashOperations.size(HASH_OPERATIONS_KEY);
@@ -51,11 +60,14 @@ class HashOperationsTest extends RedisTest {
         // delete
         hashOperations.delete(HASH_OPERATIONS_KEY, "count1", "count2", "tigger1", "tigger2", "tigger3", "notExist");
 
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "count1")).isNull();
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "count2")).isNull();
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger1")).isNull();
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger2")).isNull();
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger3")).isNull();
-        assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "notExist")).isNull();
+        assertAll(
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "count1")).isNull(),
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "count2")).isNull(),
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger1")).isNull(),
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger2")).isNull(),
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger3")).isNull(),
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "notExist")).isNull()
+        );
+
     }
 }
