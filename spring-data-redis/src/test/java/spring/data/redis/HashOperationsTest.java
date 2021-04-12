a74d88ab30548f09e7f0effc94ea019e81fccf85
kimjunseok <jnunseok@gmail.com>
2021-04-12 21:18:14 +0900

    refactor: HashOperations 테스트 함수 분리

--- spring-data-redis/src/test/java/spring/data/redis/HashOperationsTest.java
+++ spring-data-redis/src/test/java/spring/data/redis/HashOperationsTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import javax.annotation.Resource;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.redis.core.HashOperations;
 
@@ -15,9 +16,25 @@ class HashOperationsTest extends RedisTest {
     @Resource(name = "redisTemplate")
     private HashOperations<String, String, Object> hashOperations;
 
+    @BeforeEach
+    void setUp() {
+        hashOperations.put(HASH_OPERATIONS_KEY, "tigger1", "개발자");
+        hashOperations.put(HASH_OPERATIONS_KEY, "tigger2", "취준생");
+    }
+
     @Test
-    void HashOperations_Test() {
-        // increment
+    void put() {
+        hashOperations.put(HASH_OPERATIONS_KEY, "tigger3", "백수");
+
+        assertAll(
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger1")).isEqualTo("개발자"),
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger2")).isEqualTo("취준생"),
+                () ->assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger3")).isEqualTo("백수")
+        );
+    }
+
+    @Test
+    void increment() {
         hashOperations.put(HASH_OPERATIONS_KEY, "count1", "1");
         hashOperations.put(HASH_OPERATIONS_KEY, "count2", "1");
         hashOperations.increment(HASH_OPERATIONS_KEY, "count1", 1);
@@ -27,20 +44,10 @@ class HashOperationsTest extends RedisTest {
                 () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "count1")).isEqualTo("2"),
                 () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "count2")).isEqualTo("3")
         );
+    }
 
-
-        // put
-        hashOperations.put(HASH_OPERATIONS_KEY, "tigger1", "개발자");
-        hashOperations.put(HASH_OPERATIONS_KEY, "tigger2", "취준생");
-        hashOperations.put(HASH_OPERATIONS_KEY, "tigger3", "백수");
-
-        assertAll(
-                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger1")).isEqualTo("개발자"),
-                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger2")).isEqualTo("취준생"),
-                () ->assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger3")).isEqualTo("백수")
-        );
-
-        // putIfAbsent
+    @Test
+    void putIfAbsent() {
         Boolean tigger1 = hashOperations.putIfAbsent(HASH_OPERATIONS_KEY, "tigger1", "요리사");
         Boolean notExist = hashOperations.putIfAbsent(HASH_OPERATIONS_KEY, "notExist", "개발자");
 
@@ -50,23 +57,22 @@ class HashOperationsTest extends RedisTest {
                 () -> assertThat(notExist).isTrue(),
                 () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "notExist")).isEqualTo("개발자")
         );
+    }
 
-
-        // size
+    @Test
+    void size() {
         Long size = hashOperations.size(HASH_OPERATIONS_KEY);
 
-        assertThat(size).isEqualTo(6);
+        assertThat(size).isEqualTo(2);
+    }
 
-        // delete
-        hashOperations.delete(HASH_OPERATIONS_KEY, "count1", "count2", "tigger1", "tigger2", "tigger3", "notExist");
+    @Test
+    void delete() {
+        hashOperations.delete(HASH_OPERATIONS_KEY, "tigger1", "tigger2");
 
         assertAll(
-                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "count1")).isNull(),
-                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "count2")).isNull(),
                 () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger1")).isNull(),
-                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger2")).isNull(),
-                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger3")).isNull(),
-                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "notExist")).isNull()
+                () -> assertThat(hashOperations.get(HASH_OPERATIONS_KEY, "tigger2")).isNull()
         );
 
     }
