dbfb69ce9ac0d0e205fabd0ce7f264e637075057
kimjunseok <jnunseok@gmail.com>
2021-04-18 14:27:08 +0900

    feat: Post domain 생성

---
+++ spring-cache/src/main/java/spring/cache/domain/Post.java
@@ -0,0 +1,35 @@
+package spring.cache.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    protected Post() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
